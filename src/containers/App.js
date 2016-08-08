66ca91e64359c4f40da53bd88825f26bddf5b965
Justin Zimmerman <zimmy130@gmail.com>
2016-08-07 20:58:13 -0400

    modify background color

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'orange',
+    backgroundColor: '#fc4c02',
     padding: 40
   },
   text: {
