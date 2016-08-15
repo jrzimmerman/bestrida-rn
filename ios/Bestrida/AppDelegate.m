efadd8a4276d00b90ff9c880b5d5499bb1289ed3
Justin Zimmerman <zimmy130@gmail.com>
2016-08-14 20:32:46 -0400

    add codepush

--- ios/Bestrida/AppDelegate.m
+++ ios/Bestrida/AppDelegate.m
@@ -8,9 +8,12 @@
  */
 
 #import "AppDelegate.h"
+#import "CodePush.h"
 
 #import "RCTBundleURLProvider.h"
 #import "RCTRootView.h"
+
+#import "RCTLinkingManager.h"
 
 @implementation AppDelegate
 
@@ -18,7 +21,12 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  
+#ifdef DEBUG
+    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+#else
+    jsCodeLocation = [CodePush bundleURL];
+#endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Bestrida"
