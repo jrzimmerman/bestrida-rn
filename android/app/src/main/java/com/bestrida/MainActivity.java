4689d2b47e2b6902ec56b2930162fae33adb2bf6
Justin Zimmerman <zimmy130@gmail.com>
2016-09-21 22:09:07 -0400

    update android crashlytics

--- android/app/src/main/java/com/bestrida/MainActivity.java
+++ android/app/src/main/java/com/bestrida/MainActivity.java
@@ -1,8 +1,10 @@
 package com.bestrida;
 
 import com.facebook.react.ReactActivity;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.smixx.fabric.FabricPackage;
+
+import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends ReactActivity {
 
@@ -10,6 +12,13 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+    }
+
     @Override
     protected String getMainComponentName() {
         return "Bestrida";
